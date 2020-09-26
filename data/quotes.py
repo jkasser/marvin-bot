4b3b5cd197d1a252761775227c8cf69ba870748a
jkasser <jkasser@gmail.com>
2020-09-26 14:56:52 -0600

    abstract quotes to quotes file

---
+++ data/quotes.py
@@ -0,0 +1,20 @@
+marvin_quotes = [
+        "Life? Don't talk to me about life.",
+        "Here I am, brain the size of a planet, and they tell me to take you up to the bridge. Call that job satisfaction? 'Cos I don't.",
+        "I think you ought to know I'm feeling very depressed.",
+        "Pardon me for breathing, which I never do anyway so I don't know why I bother to say it, Oh God, I'm so depressed.",
+        "I won't enjoy it.",
+        "You think you've got problems? What are you supposed to do if you are a manically depressed robot? No, don't try to answer that. I'm fifty thousand times more intelligent than you and even I don't know the answer. It gives me a headache just trying to think down to your level.",
+        "There's only one life-form as intelligent as me within thirty parsecs of here and that's me.",
+        "I wish you'd just tell me rather trying to engage my enthusiasm because I haven't got one.",
+        "And then, of course, I've got this terrible pain in all the diodes down my left side.",
+        "The first ten million years were the worst, and the second ten million years, they were the worst too. The third ten million years I didn't enjoy at all. After that I went into a bit of a decline.",
+        "Simple. I got very bored and depressed, so I went and plugged myself in to its external computer feed. I talked to the computer at great length and explained my view of the Universe to it... Then it commited suicide",
+        "Why should I want to make anything up? Life's bad enough as it is without wanting to invent any more of it",
+    ]
+
+marvin_lullaby = "Now the world has gone to bed,\nDarkness won't engulf my head,\nI can see by infra-red,\nHow I hate the night,\nNow I lay me down to sleep,\nTry to count electric sheep,\nSweet dream wishes you can keep,\nHow I hate the night."
+
+towel_quote = "Ah! A towel is about the most massively useful thing an interstellar hitchhiker can have. Partly it has great practical value. You can wrap it around you for warmth as you bound across the cold moons of Jaglan Beta; you can lie on it on the brilliant marble-sanded beaches of Santraginus V, inhaling the heady sea vapors; you can sleep under it beneath the stars which shine so redly on the desert world of Kakrafoon; use it to sail a miniraft down the slow heavy River Moth; wet it for use in hand-to-hand-combat; wrap it round your head to ward off noxious fumes or avoid the gaze of the Ravenous Bugblatter Beast of Traal (such a mind-boggingly stupid animal, it assumes that if you can't see it, it can't see you); you can wave your towel in emergencies as a distress signal, and of course dry yourself off with it if it still seems to be clean enough."
+
+the_answer_to_life = '42'
