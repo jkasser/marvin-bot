6e0786f33ce2a7fd316653531622906b03122eab
jkasser <jkasser@gmail.com>
2020-09-26 14:56:58 -0600

    update main to use quotes file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import yaml
 import random
-from riot.riot_api import Riot
 import discord
 from discord.ext import commands
 from discord import Spotify
+from riot.riot_api import Riot
+from data.quotes import *
+
 
 file = open('config.yaml', 'r')
 cfg = yaml.load(file, Loader=yaml.FullLoader)
@@ -24,10 +26,13 @@ async def on_message(message):  # event that happens per any message.
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
 
     message_text = message.content.strip().lower()
+    channel = message.channel
     if 'hi marvin' in message_text:
-        channel = message.channel
         await channel.send(f'Why bother, {message.author.name}...')
-
+    elif 'towel' in message_text:
+        await channel.send(towel_quote)
+    elif 'life' in message_text:
+        await channel.send(the_answer_to_life)
     await bot.process_commands(message)
 
 
@@ -41,24 +46,12 @@ async def on_member_join(member):
 
 @bot.command(name='marvin', help='Check in on Marvin the depressed robot!')
 async def marvin_quote(ctx):
-    marvin_quotes = [
-        "Life? Don't talk to me about life.",
-        "Here I am, brain the size of a planet, and they tell me to take you up to the bridge. Call that job satisfaction? 'Cos I don't.",
-        "I think you ought to know I'm feeling very depressed.",
-        "Pardon me for breathing, which I never do anyway so I don't know why I bother to say it, Oh God, I'm so depressed.",
-        "I won't enjoy it.",
-        "You think you've got problems? What are you supposed to do if you are a manically depressed robot? No, don't try to answer that. I'm fifty thousand times more intelligent than you and even I don't know the answer. It gives me a headache just trying to think down to your level.",
-        "There's only one life-form as intelligent as me within thirty parsecs of here and that's me.",
-        "I wish you'd just tell me rather trying to engage my enthusiasm because I haven't got one.",
-        "And then, of course, I've got this terrible pain in all the diodes down my left side."
-    ]
     response = random.choice(marvin_quotes)
     await ctx.send(response)
 
 
 @bot.command(name='lullaby', help='Let Marvin read you a lullaby!')
 async def marvin_lullaby(ctx):
-    marvin_lullaby = "Now the world has gone to bed,\nDarkness won't engulf my head,\nI can see by infra-red,\nHow I hate the night,\nNow I lay me down to sleep,\nTry to count electric sheep,\nSweet dream wishes you can keep,\nHow I hate the night."
     await ctx.send(marvin_lullaby)
 
 
