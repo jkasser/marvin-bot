cea97c2445c0b1628e415fb135455875c4043e7a
jkasser <jkasser@gmail.com>
2020-09-30 15:26:20 -0600

    another gif!

--- main.py
+++ main.py
@@ -52,6 +52,8 @@ async def on_message(message):  # event that happens per any message.
             await channel.send(thumb_quote)
         elif 'shut up' in message_text or 'be quiet' in message_text or 'stfu' in message_text:
             await channel.send(file=discord.File('./media/shut_up.gif'))
+        elif 'wtf' in message_text or 'what the' in message_text:
+            await channel.send(file=discord.File('./media/wtf.gif'))
     await bot.process_commands(message)
 
 
