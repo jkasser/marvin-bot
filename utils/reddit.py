53a7172e3f78fdca7220458c4f36813a911c15be
jkasser <jkasser@gmail.com>
2020-09-30 10:51:55 -0600

    rewrite reddit class

--- utils/reddit.py
+++ utils/reddit.py
@@ -1,14 +1,43 @@
 import praw
+from sqlite3 import Error
+from utils.db import MarvinDB
 
 
-class MarvinReddit():
+class MarvinReddit(MarvinDB):
 
-    def __init__(self, client_id, client_secret,):
+    TABLE_NAME = 'reddit'
+    REDDIT_TABLE = f"""CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+        id integer PRIMARY KEY,
+        post_id integer NOT NULL
+    );"""
+
+    INSERT_POST = f"""INSERT INTO {TABLE_NAME}(post_id) VALUES(?)"""
+
+    CHECK_IF_EXISTS = f"""SELECT EXISTS(SELECT * FROM {TABLE_NAME} WHERE post_id=? LIMIT 1)"""
+
+    def __init__(self, client_id, client_secret):
+        super().__init__()
         self.reddit = praw.Reddit(
             client_id=client_id,
             client_secret=client_secret,
             user_agent="Marvin Bot 1.0 by /u/onebagoneworld",
         )
+        try:
+            self.create_table(self.conn, self.REDDIT_TABLE)
+        except Error as e:
+            print(e)
+
+    def add_post_id_to_db(self, post_id):
+        self.insert_query(self.INSERT_POST, (post_id,))
+
+    def check_if_post_exists(self, post_id):
+        cur = self.conn.cursor()
+        results = cur.execute(self.CHECK_IF_EXISTS, (post_id,))
+        results = results.fetchone()[0]
+        if results == 0:
+            return False
+        else:
+            return True
 
     def get_travel_stream(self, limit=1):
        submissions = [submission for submission in self.reddit.multireddit("OneBagOneWorld", "OneBagOneWorld").hot(limit=limit)]
@@ -16,9 +45,15 @@ class MarvinReddit():
             post_list = self.parse_stream(submissions)
             return post_list
 
-
     def parse_stream(self, stream):
         post_list = []
         for submission in stream:
-            post_list.append(f'{submission.title}\n{submission.selftext}\n{submission.url}')
+            if not submission.stickied:
+                post_id = submission.id
+                if self.check_if_post_exists(post_id):
+                    # if we already have the post, just skip it
+                    continue
+                else:
+                    self.add_post_id_to_db(submission.id)
+                    post_list.append(f'{submission.title}\n{submission.selftext}\n{submission.url}')
         return post_list
