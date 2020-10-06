2938c1acd6141bf8599481016b26b60ee4159eaa
jkasser <jkasser@gmail.com>
2020-10-06 13:40:58 -0600

    add some comments

--- main.py
+++ main.py
@@ -510,6 +510,7 @@ async def check_and_update_latest_assets_version():
     api_current_version, cdn = rito.get_latest_data_version()
     if rito.check_if_assets_current_version_exists():
         assets_db_version = rito.get_current_assets_version_from_db()[0]
+        # See if the api version is greater than our current one
         if int(''.join(api_current_version.split('.'))) > int(''.join(assets_db_version.split('.'))):
             await api_updates_channel.send(f'Our current version: {assets_db_version} is out of date!\nDownloading latest version: {api_current_version}')
             # Update our local assets
@@ -517,10 +518,12 @@ async def check_and_update_latest_assets_version():
             # Now Update it in the DB
             rito.update_assets_current_version(current_version=api_current_version)
             await api_updates_channel.send(f'We are now using LoL assets version: {api_current_version}')
+        # otherwise if they are equal then just say we are on the most current version
         elif int(''.join(api_current_version.split('.'))) == int(''.join(assets_db_version.split('.'))):
             await api_updates_channel.send(f'We are on the most current LoL assets version: {assets_db_version}')
             return
     else:
+        # If the field doesn't exist then download the latest version
         rito.download_new_assets(cdn, api_current_version)
         # update our local assets
         # Add it to the DB
