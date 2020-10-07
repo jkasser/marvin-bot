875873d2ae373dea549930cc7d9ae047f63405aa
jkasser <jkasser@gmail.com>
2020-10-07 17:30:14 -0600

    add helper methods and str compare

--- utils/helper.py
+++ utils/helper.py
@@ -1,4 +1,6 @@
 import calendar
+import re
+import difflib
 
 
 def get_user_friendly_date_from_string(date_string: str) -> str:
@@ -7,3 +9,17 @@ def get_user_friendly_date_from_string(date_string: str) -> str:
 
 def get_slug_from_url(url):
     return f'{url.strip("/").split("/")[-1]}'
+
+
+def parse_question_from_link(text):
+    question = re.compile(r'(?<=\>).*(?=\<)')
+    return question.search(text).group()
+
+
+def parse_href_from_string(text):
+    url = re.compile(r'(?<=\")http.+?(?=\\\")')
+    return url.search(text).group()
+
+
+def compare_answers(correct, provided):
+    return float(str(difflib.SequenceMatcher(None, correct, provided).ratio())[:5])
