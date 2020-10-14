56a30296d3d73f5ee4967ed6b9ea11c08e45cf5d
jkasser <jkasser@gmail.com>
2020-10-14 07:51:43 -0600

    comment out assets update, test it locally

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ async def on_ready():  # method expected by client. This runs once when connecte
     check_reddit_lol_stream.start()
     check_reddit_travel_stream.start()
     check_the_news.start()
-    check_and_update_latest_assets_version.start()
+    # check_and_update_latest_assets_version.start()
     get_rito_status.start()
     check_for_reminders.start()
 
