e78389730b02159a2743f3c7021442ff0c9ec029
jkasser <jkasser@gmail.com>
2020-10-15 14:02:34 -0600

    add to do database

--- cogs/todo.py
+++ cogs/todo.py
@@ -1,6 +1,6 @@
 from utils.db import MarvinDB
-import sqlite3
-from discord.ext import commands
+import json
+from discord.ext import commands, tasks
 
 
 class ToDo(MarvinDB, commands.Cog):
@@ -9,21 +9,49 @@ class ToDo(MarvinDB, commands.Cog):
     TODO_TABLE = f"""CREATE TABLE IF NOT EXISTS {TODO_TABLE_NAME} (
         id integer PRIMARY KEY,
         user text NOT NULL,
-        item text NOT NULL,
-        position integer NOT NULL
+        item text NOT NULL
     );"""
+    INSERT_TO_DO_FOR_USER = f"""INSERT INTO {TODO_TABLE_NAME} (user, item) VALUES(?,?);"""
+    GET_TO_DO_LISTS = f"""SELECT * FROM {TODO_TABLE_NAME}"""
+    UPDATE_TO_DO_FOR_USER = f"""UPDATE {TODO_TABLE_NAME} SET item=? WHERE user=?"""
+    CHECK_IF_EXISTS = f"""SELECT EXISTS(SELECT * FROM {TODO_TABLE_NAME} WHERE user=? LIMIT 1)"""
 
     def __init__(self, bot):
         super(ToDo, self).__init__()
-        self.to_do = {}
+        self.to_do = dict()
         self.bot = bot
         self.create_table(self.conn, self.TODO_TABLE)
+        # populate the to do list at this point
+        to_do_lists = self.get_to_do_lists()
+        if len(to_do_lists) > 0:
+            for x in to_do_lists:
+                self.to_do[x[1]] = json.loads(x[2])
+        # now start the task loop to keep this updated, once every 10 minutes
+        self.update_to_do.start()
 
+    def check_if_user_exists(self, user):
+        cur = self.conn.cursor()
+        results = cur.execute(self.CHECK_IF_EXISTS, (user,))
+        results = results.fetchone()[0]
+        if results == 0:
+            return False
+        else:
+            return True
+
+    def insert_user_and_to_do_list(self, user: str, to_do_list: str):
+        self.insert_query(self.INSERT_TO_DO_FOR_USER, (user, to_do_list,))
+
+    def update_to_do_list_for_user(self, user: str, to_do_list: str):
+        self.update_query(self.UPDATE_TO_DO_FOR_USER, (to_do_list, user,))
+
+    def get_to_do_lists(self):
+        results = self.get_query(self.GET_TO_DO_LISTS)
+        return results
 
     @commands.command(name='todoadd', help="Add an item to your to do list!")
-    async def addto_do(self, ctx, *item):
+    async def add_to_do(self, ctx, *item):
         item_to_add = " ".join(item)
-        user = ctx.author
+        user = str(ctx.author)
         channel = await ctx.author.create_dm()
         if not user in self.to_do.keys():
             self.to_do[user] = []
@@ -36,7 +64,7 @@ class ToDo(MarvinDB, commands.Cog):
 
     @commands.command(name='todoremove', help="Remove an item by its position in your to do list!")
     async def remove_self(self, ctx, item):
-        user = ctx.author
+        user = str(ctx.author)
         channel = await ctx.author.create_dm()
         if not user in self.to_do.keys():
             await channel.send('You do not currently have a to do list!')
@@ -53,9 +81,37 @@ class ToDo(MarvinDB, commands.Cog):
 
     @commands.command(name='todoclear', help="Clear your to do list!")
     async def clear_to_do(self, ctx):
-        user = ctx.author
+        user = str(ctx.author)
         self.to_do[user] = []
         await ctx.send('Your to do list has been cleared!')
+
+    @commands.command(name='todoget', help="Retrieve your current to do list via DM!")
+    async def get_to_do(self, ctx):
+        user = str(ctx.author)
+        channel = await ctx.author.create_dm()
+        if not user in self.to_do.keys():
+            self.to_do[user] = []
+            await channel.send('Your to do list has been created!\nYour to do list is currently empty!')
+        elif len(self.to_do[user]) == 0:
+            await channel.send('You have no items in your to do list!')
+        else:
+            new_line = '\n'
+            lines = [f"{self.to_do[user].index(x) + 1}. {x}" for x in self.to_do[user]]
+            await channel.send(f"{new_line}".join(lines))
+
+    @tasks.loop(minutes=10)
+    async def update_to_do(self):
+        for user, todo, in self.to_do.items():
+            user = str(user)
+            if self.check_if_user_exists(user):
+                # convert it to json and store it as a string, we will read it back with json.loads
+                self.update_to_do_list_for_user(user, json.dumps(todo))
+            else:
+                self.insert_user_and_to_do_list(user, json.dumps(todo))
+
+    @update_to_do.before_loop
+    async def before_update_to_do(self):
+      await self.bot.wait_until_ready()
 
 
 def setup(bot):
