86ce8a3b13ad70092aa303866b347aaf28b9b6a0
jkasser <jkasser@gmail.com>
2020-10-21 16:19:45 -0600

    reduce to 1 min for testing

--- cogs/address_book.py
+++ cogs/address_book.py
@@ -371,7 +371,7 @@ class AddressBook(commands.Cog, SubscriptionsDB):
                            '!subsettz to set your timezone. Then you will want to add contacts before you have'
                            'something to update.')
 
-    @tasks.loop(minutes=5)
+    @tasks.loop(minutes=1)
     async def insert_or_update_contacts_in_database(self):
         for user, info in self.address_book.items():
             if "user_id" not in info.keys():
