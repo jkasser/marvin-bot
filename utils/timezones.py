d7adf57408dfdd4bf271bc7969ecdcd20a9e890a
jkasser <jkasser@gmail.com>
2020-10-20 20:50:30 -0600

    pep8 top level function spacing

--- utils/timezones.py
+++ utils/timezones.py
@@ -1,14 +1,18 @@
 import pytz
 from datetime import datetime
 
+
 _timezones = pytz.all_timezones
+
 
 def get_all_timezones() -> list:
     return _timezones
 
+
 def get_possible_timezones(user_tz: str) -> list:
     possibilities = [timezone for timezone in _timezones if user_tz.lower() in timezone.lower()]
     return possibilities
+
 
 def check_if_timezone_match(possibilities: list) -> bool:
     if len(possibilities) == 1:
@@ -16,12 +20,14 @@ def check_if_timezone_match(possibilities: list) -> bool:
     else:
         return False
 
+
 def get_time_in_specified_timezone(user_tz: str):
     try:
         user_tz = pytz.timezone(user_tz)
         return datetime.now(user_tz)
     except pytz.exceptions.UnknownTimeZoneError:
         print(f'Your timezone: {user_tz}, was not valid! Please try again')
+
 
 def get_date_from_epoch(timestamp: int) -> datetime:
     ts = datetime.fromtimestamp(int(timestamp))
