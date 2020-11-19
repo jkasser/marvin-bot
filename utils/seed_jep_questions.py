eda5089449c214d495e2d2e9f73e137387079448
jkasser <jkasser@gmail.com>
2020-11-19 12:19:50 -0700

    run one script test 2

--- utils/seed_jep_questions.py
+++ utils/seed_jep_questions.py
@@ -1,10 +1,12 @@
 import os
 import json
+import yaml
+import discord
+from discord.ext import commands
 from cogs.jeopardy import Jeopardy
-from utils.db import MarvinDB
 
 
-class InsertQuestions(MarvinDB):
+class InsertQuestions(Jeopardy):
 
     def insert_questions(self):
         json_file = os.path.dirname(os.path.dirname(__file__)) + '/data/questions.json'
@@ -28,4 +30,9 @@ class InsertQuestions(MarvinDB):
                 self.conn.commit()
 
 if __name__ == '__main__':
-    InsertQuestions().insert_questions()
+    file = open('../config.yaml', 'r')
+    cfg = yaml.load(file, Loader=yaml.FullLoader)
+    token = cfg["disc"]["token"]
+    intents = discord.Intents().all()
+    bot = commands.Bot(command_prefix=cfg["disc"]["prefix"], intents=intents)
+    InsertQuestions(bot).insert_questions()
