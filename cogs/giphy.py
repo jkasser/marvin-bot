bfeca96e40feb5bf25919bdcfe52e2d8a591e6b9
Jeff Kasser <jkasser@gmail.com>
2021-08-25 10:58:12 +0200

    better error handling for giphy, also switch back to popular instead of generic search

--- cogs/giphy.py
+++ cogs/giphy.py
@@ -19,21 +19,23 @@ class Giphy(commands.Cog):
     @commands.command(name='gif', help='Sends the top trending gif for the provided keyword(s). If no search term is '
                                        'provided then I will send a random gif', pass_context=True)
     async def post_trending(self, ctx, *search):
-        print(search)
-        embed = discord.Embed(colour=discord.Colour.blue())
-        async with aiohttp.ClientSession() as session:
-            if search == ():
-                response = await session.get(f'{self.base_url}/random?api_key={self.key}')
-                data = await response.json()
-                embed.set_image(url=data['data']['images']['original']['url'])
-            else:
-                search = '+'.join(search)
-                response = await session.get(
-                    f'{self.base_url}/search?q={search}&api_key={self.key}&limit={self.gif_results_limit}')
-                data = await response.json()
-                choice = random.choice(range(0, self.gif_results_limit))
-                embed.set_image(url=data['data'][int(choice)]['images']['original']['url'])
-        await ctx.send(embed=embed)
+        try:
+            embed = discord.Embed(colour=discord.Colour.blue())
+            async with aiohttp.ClientSession() as session:
+                if search == ():
+                    response = await session.get(f'{self.base_url}/random?api_key={self.key}')
+                    data = await response.json()
+                    embed.set_image(url=data['data']['images']['original']['url'])
+                else:
+                    search = '+'.join(search)
+                    response = await session.get(
+                        f'{self.base_url}/trending?q={search}&api_key={self.key}&limit={self.gif_results_limit}')
+                    data = await response.json()
+                    choice = random.choice(range(0, self.gif_results_limit))
+                    embed.set_image(url=data['data'][int(choice)]['images']['original']['url'])
+            await ctx.send(embed=embed)
+        except (IndexError, KeyError):
+            await ctx.send('I could\'nt find any matching results! Please try again.')
 
 
 def setup(bot):
