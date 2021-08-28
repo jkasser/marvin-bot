60ccebc3b85e2e917a1cd0de891d255db04720b8
Jeff Kasser <jkasser@gmail.com>
2021-08-28 11:01:33 +0200

    add the ability to tell jokes

---
+++ cogs/jokes.py
@@ -0,0 +1,63 @@
+import discord
+import random
+import json
+import requests
+import os
+import asyncio
+import yaml
+from assets.data.quotes import *
+from discord.ext import commands
+from concurrent.futures.thread import ThreadPoolExecutor
+
+
+class MarvinStandup(commands.Cog):
+    JOKES_BASE_DIR = './assets/data/'
+    JOKES_FILES = {
+        "wocka": "wocka.json",
+        "stupid": "stupidstuff.json",
+        "reddit": "reddit.json"
+    }
+
+    def __init__(self, bot):
+        self.bot = bot
+    # file = open('config.yaml', 'r')
+    # cfg = yaml.load(file, Loader=yaml.FullLoader)
+    # env = os.environ.get('ENV', 'NOT SET')
+    # self.joke_channel = cfg["disc"][env]["booster_lounge_channel"]
+        self.jokes_list = {
+            "wocka": self.load_wocka_jokes(),
+            "reddit":  self.load_reddit_jokes(),
+            "stupid": self.load_stupid_jokes()
+        }
+
+
+    def load_reddit_jokes(self):
+        with open(self.JOKES_BASE_DIR + self.JOKES_FILES["reddit"]) as reddit_json:
+            reddit_jokes = json.load(reddit_json)
+            return reddit_jokes
+
+    def load_stupid_jokes(self):
+        with open(self.JOKES_BASE_DIR + self.JOKES_FILES["stupid"]) as stupid_json:
+            stupid_jokes = json.load(stupid_json)
+            return stupid_jokes
+
+    def load_wocka_jokes(self):
+        with open(self.JOKES_BASE_DIR + self.JOKES_FILES["wocka"]) as wocka_json:
+            wocka_jokes = json.load(wocka_json)
+            return wocka_jokes
+
+    @commands.command(name='joke', help='Want to hear something funny?')
+    async def tell_joke(self, ctx):
+        joke_category = random.choice(list(self.jokes_list.keys()))
+        joke_choice = random.choice(self.jokes_list[joke_category])
+        if joke_category.lower() == 'reddit':
+            await ctx.send(joke_choice["title"])
+            await asyncio.sleep(1)
+            await ctx.send(joke_choice["body"])
+        else:
+            # Wocka and Stupid json files have the same format
+            await ctx.send(joke_choice["body"])
+
+
+def setup(bot):
+    bot.add_cog(MarvinStandup(bot))
